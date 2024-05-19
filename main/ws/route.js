0bbd7071f906c2aba01238958db0f843a233e205
artistXenon <xtramainpp@gmail.com>
2024-05-19 16:26:18 +0900

    show calendar on ws

--- main/ws/route.js
+++ main/ws/route.js
@@ -52,7 +52,7 @@ async function onToday(data) {
     }
 //     if google { give google today calendar }
 //     else { app config qrcode }
-    return undefined;
+    return calendar;
 }
 
 function onPing(data) {
